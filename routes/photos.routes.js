55779f34d5b1dfe6b15d026daea690d92d287adf
nandah4 <nyustira6@gmail.com>
2024-01-26 22:21:48 +0700

    Routes like and comment

--- routes/photos.routes.js
+++ routes/photos.routes.js
@@ -4,6 +4,8 @@ const photosRoutes = express.Router();
 const { getPhoto, getPhotoById, getPhotoByIdUser, createPhoto, updatePhotoById, deletePhotoById } = require('../controllers/photos.controllers');
 const { fileStorage, fileFilter } = require('../middlewares/photoMiddleware');
 const { authMiddleware } = require('../middlewares/authMiddleware');
+const { createComentarById, deleteComentarById } = require("../controllers/comentar.controllers")
+const { createLikeByIdUser, deleteLikeByIdUser } = require("../controllers/like.controllers");
 
 photosRoutes.get("/", authMiddleware, getPhoto); // all photo
 photosRoutes.get("/:photoId", authMiddleware, getPhotoById); // by id photo
@@ -11,5 +13,13 @@ photosRoutes.post("/user", authMiddleware, getPhotoByIdUser); // by id user
 photosRoutes.post("/", authMiddleware, multer({ storage: fileStorage, fileFilter }).single('locationFile'), createPhoto);
 photosRoutes.delete("/:photoId", authMiddleware, deletePhotoById);
 photosRoutes.put("/:photoId", authMiddleware, updatePhotoById);
+
+// ROUTES COMMENT
+photosRoutes.post("/:photoId/comentar", authMiddleware, createComentarById);
+photosRoutes.delete(":/comentarId", authMiddleware, deleteComentarById);
+
+// ROUTES LIKE
+photosRoutes.post("/:photoId/like", authMiddleware, createLikeByIdUser);
+photosRoutes.delete("/:photoId/like", authMiddleware, deleteLikeByIdUser);
 
 module.exports = { photosRoutes };
